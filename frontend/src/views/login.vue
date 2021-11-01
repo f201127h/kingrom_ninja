52e08f320680dccaab8428b9f5003a344e84a5c2
f201127h <76505189+f201127h@users.noreply.github.com>
2021-11-01 20:19:20 +0800

    Update login.vue

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -72,7 +72,6 @@
         <div class="card-body text-base leading-6">
           <p>PC用户建议使用浏览器<a style="" href="https://www.juan920.com/1310.html" target="_blank" id="kingrom">Cookie获取</a>获取cookie并在下方填写。</p>
           <p>手机用户可以使用Alook浏览器登录<a style="" href="https://m.jd.com/" target="_blank" id="jd">JD官网</a>，并在菜单-工具箱-开发者工具-Cookies中获取（Android和iPhone通用）。</p>
-          <p>另外也可以使用抓包工具（iPhone：Stream，Android：HttpCanary）抓取京东app的ck，要注意pt_key和pt_pin字段是以app_open开头的。</p>
           <p>安卓用户可以直接下载此软件获取cookie<a style="" href:"https://wwe.lanzoui.com/icpaIvxnpif" target="_blank" id="jd">APP下载</a>，密码:awc5</p>
           <p>cookie直接填入输入框即可，Ninja会自动正则提取pt_key和pt_pin。</p>
         </div>
